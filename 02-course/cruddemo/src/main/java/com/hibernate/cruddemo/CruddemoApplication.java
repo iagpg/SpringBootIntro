b2c0eeca35443ee446d0d15f60656861691cf803
iagpg <iago.pereirasp17@gmail.com>
2025-05-30 13:48:21 -0300

    new project JPA Hibernate Crud with mysql

---
+++ 02-course/cruddemo/src/main/java/com/hibernate/cruddemo/CruddemoApplication.java
@@ -0,0 +1,22 @@
+package com.hibernate.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	//will run after beans have been created
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {	
+			System.out.println("CommandLineRunner executed with arguments: ");
+		};
+	}
+}
