c54509f772cc67d6a61535f8d4007504f94bd88e
iagopg <iago.pereirasp17@gmail.com>
2025-07-16 15:35:15 -0300

    removed jpa configuration on producer and added jpa configuration to consumer

--- Java Spring Kfka/library-events-library/Queries/database and table.sql
+++ Java Spring Kfka/library-events-library/Queries/database and table.sql
@@ -9,6 +9,5 @@ CREATE TABLE libraryStore (
 id int NOT NULL auto_increment PRIMARY KEY,
 first_name varchar(45) DEFAULT NULL,
 last_name varchar(45) DEFAULT NULL,
-email varchar(45) DEFAULT NULL)
-
-ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+email varchar(45) DEFAULT NULL
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
